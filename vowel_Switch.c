4485c3c3d163da3b759545b448bf23db02213778
Rajnish Mangla <rajnishmangla009@gmail.com>
2026-09-20 22:39:57 +0530

    Added a program Vowel of Consonent with Switch Statement

---
+++ vowel_Switch.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main() {
+
+  char ch;
+
+  printf("Enter a Character: ");
+  scanf("%c", &ch);
+
+  switch (ch) {
+  case 'A':
+  case 'E':
+  case 'I':
+  case 'O':
+  case 'U':
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o':
+  case 'u':
+    printf("Vowel");
+    break;
+
+  default:
+    printf("Consonent");
+    break;
+  }
+}
